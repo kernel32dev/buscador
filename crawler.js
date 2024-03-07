ca9bebafeacd95aac6a75b635bffc744be318565
kernel32dev <kernel32dev@gmail.com>
2024-03-07 10:50:38 -0300

    fix caso de erro + adicionado comentarios

--- crawler.js
+++ crawler.js
@@ -5,7 +5,8 @@ import { load } from "cheerio";
 /** encontra todas as páginas e lista elas em um array
  *
  * @param {string} entry_point 
- * @param {number} [max_depth] */
+ * @param {number} [max_depth]
+ */
 export async function crawl_collect(entry_point, max_depth) {
     /** @type {{url: string, $: import("cheerio").CheerioAPI}[]} */
     let list = [];
@@ -25,7 +26,7 @@ export async function crawl(entry_point, callback, max_depth) {
     /** o conjunto de todos os links já visitados @type {Set<string>} */
     let found = new Set();
 
-    /** chama a primeira função recursiva */
+    // chama a primeira função recursiva
     await crawl_one(entry_point, 0);
 
     /** função recursiva que trata o download e analise de um link
@@ -34,16 +35,32 @@ export async function crawl(entry_point, callback, max_depth) {
      * @param {number} depth 
      */
     async function crawl_one(url, depth) {
+
+        // não faz nada se o link já foi visitado
         if (found.has(url)) return;
+
+        // marca o link como visitado
         found.add(url);
+
+        // baixa o html
         let html = await download_html(url);
+        if (html == "") return;
+
+        // parsa o html
         let document = load(html, {}, true);
+
+        // extrai os links e processa links relativos
         let links = document("a")
             .get()
             .map(x => x.attribs.href)
             .filter(x => x)
             .map(x => new URL(x, url).toString());
+
+        // chama o callback com a url e o html parsado
         callback(url, document);
+
+        // recursivamente trata todos os links e espera eles terminarem
+        // mas apenas se a profundidade deixar
         if (max_depth === undefined || depth < max_depth) {
             await Promise.all(links.map(x => crawl_one(x, depth + 1)));
         }
@@ -59,6 +76,7 @@ async function download_html(url) {
     let response = await fetch(url, { redirect: "follow" }).catch(e => (console.warn(e), null));
     if (response == null) return "";
     let mimetype = response.headers.get("Content-Type");
-    if (mimetype == null || mimetype.indexOf("text/html") == -1 || response.status < 200 || response.status >= 300) return "";
+    if (mimetype == null || mimetype.indexOf("text/html") == -1) return "";
+    if (response.status < 200 || response.status >= 300) return "";
     return await response.text().catch(e => (console.warn(e), ""));
 }
