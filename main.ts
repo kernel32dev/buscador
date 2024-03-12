08d134c0ce5a811840fe71ed5b62f8ea82f35beb
kernel32dev <kernel32dev@gmail.com>
2024-03-12 08:56:39 -0300

    adicionado mais um link interessante

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import { index } from "./indexer.js";
 
 const ENTRY_POINT = "https://kernel32dev.github.io/hosp-pi/matrix.html";
+//const ENTRY_POINT = "https://iep.utm.edu/";
 //const ENTRY_POINT = "https://sites.google.com/site/steveyegge2/singleton-considered-stupid";
 //const ENTRY_POINT = "https://sites.google.com/site/steveyegge2/home";
 //const ENTRY_POINT = "http://127.0.0.1:5500/matrix.html";
